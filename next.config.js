9a7e69b551ec8af7dc7bef134f9894e6e05b1881
Mohini Gupta <124530250+mohini-sep@users.noreply.github.com>
2025-04-21 04:59:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: true, // Add this to ensure paths have a trailing slash
+  assetPrefix: '/Next.js-021-Trade-task/', // Set this to match your GitHub repository name
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
